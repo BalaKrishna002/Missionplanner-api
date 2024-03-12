688e6a357e1a7ee2dc14372affb031f3ea961ed9
Bala Krishna <balumakineni8008@gmail.com>
2024-03-12 14:41:31 +0530

    added defualt message after disarm

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
-let tdata = null;
-let messages = null;
+let tdata = "Mission Disarmed/Not yet Started!";
+let messages = "Mission Disarmed/Not yet Started!";
+let waypoints = null;
 
 // POST endpoint for telemetry data
 app.post("/tdata", (req, res) => {
@@ -21,8 +22,12 @@ app.get('/tdata-data', (req, res) => {
     if (tdata) {
         if(tdata.Target.input.prearmstatus) res.status(200).json(tdata);
         else {
-            console.log("Mission Disarmed/Not yet Started!");
-            res.status(200).json({"message":"Mission Disarmed/Not yet Started!"});
+            //console.log("Mission Disarmed/Not yet Started!");
+            setTimeout(() => {
+                tdata.m_StringValue = "Mission Disarmed/Not yet Started!";
+                //console.log(myVariable); // This will be executed after 3 seconds
+            }, 1000*60*60);
+            res.status(200).json({"message":tdata});
         }
     } else {
         res.status(404).json({ error: 'No data available' });
@@ -38,11 +43,36 @@ app.post("/messages", (req, res) => {
 // GET endpoint for messages
 app.get("/messages-data", (req, res) => {
     if (messages) {
-        res.status(200).json(messages);
+        if(tdata.Target.input.prearmstatus) res.status(200).json(messages);
+        else {
+            //console.log("Mission Disarmed/Not yet Started!");
+            setTimeout(() => {
+                messages.m_StringValue = "Mission Disarmed/Not yet Started!";
+                //console.log(myVariable); // This will be executed after 3 seconds
+            }, 1000*60*60);
+            res.status(200).json({"message":messages});
+        }
+        
     } else {
         res.status(404).json({ error: 'No data available' });
     }
 });
+
+
+// POST Request for waypoints
+app.post("/waypoints",(req,res)=>{
+    waypoints = req.body;
+    res.status(200).json({"message":"Data recieved"});
+})
+
+//GET request for waypoints
+app.get("/waypoints-data",(req,res)=>{
+    if(waypoints) {
+        res.status(200).json(waypoints);
+    }else {
+        res.status(404).json({ error: 'No data available' });
+    }
+})
 
 // Start the server
 app.listen(port, () => {
